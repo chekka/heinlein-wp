e5987a1ac2d385c95406317d5402d9d36cf3584f
Johannes Tassilo Gruber <jgruber@d-ht.com>
2024-02-27 21:16:41 +0100

    style adjustments

--- htdocs/wp-content/themes/heinlein/footer.php
+++ htdocs/wp-content/themes/heinlein/footer.php
@@ -1,14 +1,5 @@
 		<footer class="site-footer">
 			<div class="container">
-			<?php 
-				wp_nav_menu(array(
-					'theme_location' => 'footer-menu',
-					'container' => 'nav',
-					'container_class' => 'footer-menu',
-					'menu_class' => 'nav-menu',
-					'menu_id' => 'primary-menu'
-				));
-			?>
 			</div>
 		</footer>
 
