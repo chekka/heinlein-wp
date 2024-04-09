593d82d2f51612dea7d55f435443e6bb8de93a78
Johannes Tassilo Gruber <jgruber@d-ht.com>
2024-04-09 16:24:07 +0200

    remove wp-content files for git

--- htdocs/wp-content/wflogs/config.php
+++
@@ -1,6 +0,0 @@
-<?php exit('Access denied'); __halt_compiler(); ?>
-******************************************************************
-This file is used by the Wordfence Web Application Firewall. Read 
-more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
-******************************************************************
-a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1713161065;s:7:"authKey";s:64:"?Q-cm%X8<-/CMICmL6E{Vgo.6; wF/Cv^TJ``$/^ofhYqJ{~ZUheAl102A*#rAIt";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:2554;}
