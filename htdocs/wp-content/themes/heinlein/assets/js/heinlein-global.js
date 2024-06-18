12557bab7cfa8063176ccc6c64be10edcceaddcd
jgruber <jgruber@d-ht.com>
2024-06-18 11:43:23 +0200

    2024-06-18--1143

--- htdocs/wp-content/themes/heinlein/assets/js/heinlein-global.js
+++ htdocs/wp-content/themes/heinlein/assets/js/heinlein-global.js
@@ -10,13 +10,37 @@
     $('body').attr({ 'data-width':$winW });
   });
 
+  $(window).on('scroll', function () {
+    if ($(window).width() > 574) {
+      t = 0;
+    } else {
+      t = 50;
+    }
+    if ($(this).scrollTop() > t) {
+      $('body').addClass('scrolled');
+      $('.site-header').addClass('active');
+    } else {
+      $('body').removeClass('scrolled');
+      $('.site-header').removeClass('active');
+    }
+    var scroll = $(window).scrollTop();
+    position = scroll;
+  });
+
   $(document).on('ready',function(){
 
     // Video popup
     $('.pum-theme-186').each(function(){
-      $video_url = $('a.pum-trigger').attr('href');
-
-      if($video_url){}
+      
+      $video_url = $('iframe').attr('src');
+      
+      if($video_url.includes( 'vimeo' )){
+        $(this).on('pumBeforeOpen', function () {
+          var $iframe = $('iframe', $(this));
+          var src = $iframe.prop('src');
+          $iframe.prop('src', '').prop('src', src + '&autoplay=1'); // Add &muted=1 if needed.
+        });
+      }
 
       $(this).on('pumBeforeOpen', function () {
         var video_src = $(this).find('video source').attr('src');
