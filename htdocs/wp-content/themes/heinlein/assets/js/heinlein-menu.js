8a563d5828dd0325b0cd2d8376f03e749c250370
jgruber <jgruber@d-ht.com>
2024-06-28 08:35:04 +0200

    2024-06-28--0835

--- htdocs/wp-content/themes/heinlein/assets/js/heinlein-menu.js
+++ htdocs/wp-content/themes/heinlein/assets/js/heinlein-menu.js
@@ -27,7 +27,7 @@
     $('.mainnav-desktop:not(#menu-produkte, #menu-products)').each(function () {
       $(this).find('.panel-grid-cell .widget_sow-image > div:not(.no-target)').each(function(){
         var title = $(this).find('.sow-image-container img').attr('alt');
-        var target = title.toLowerCase().replace(' ','-').replace('ü','ue');
+        var target = title.toLowerCase().replace(' ','-').replace('ä','ae').replace('ü','ue');
         var url = $(this).find('.sow-image-container a').attr('href');
         var new_url = url + '#' + target;
         $(this).find('.sow-image-container a').attr('href', new_url);      
@@ -38,7 +38,7 @@
       $('#desktop-menu').addClass('show');
     });
 
-    $('.site-header').on('mouseleave', function () {
+    $('.site-header').on('click mouseleave', function () {
       $('#desktop-menu').removeClass('show');
     });
 
