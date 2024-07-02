83fb5445fd01b0a282d89be88074c8d32e638ac2
jgruber <jgruber@d-ht.com>
2024-07-02 22:23:41 +0200

    sticky social banner

--- htdocs/wp-content/themes/heinlein/footer.php
+++ htdocs/wp-content/themes/heinlein/footer.php
@@ -8,6 +8,24 @@
 			</div>
 		</footer>
 
+		<div id="block-stickysocialbanner" class="">
+			<div>
+				<a href="https://www.youtube.com/@heinleinplastik-technik923" rel=" noopener" target="_blank">
+					<img alt="Heinlein auf Youtube" src="/wp-content/themes/heinlein/assets/images/icons/icon-social-youtube.svg" width="70">
+				</a>
+			</div>
+			<div>
+				<a href="https://www.instagram.com/heinlein_plastik_technik/" rel=" noopener" target="_blank">
+					<img alt="Heinlein auf Instagram" src="/wp-content/themes/heinlein/assets/images/icons/icon-social-insta.svg" width="70">
+				</a>
+			</div>
+			<div>
+				<a href="https://www.linkedin.com/company/heinlein-plastik-technik-gmbh/" rel=" noopener" target="_blank">
+					<img alt="Heinlein auf Linkedin" src="/wp-content/themes/heinlein/assets/images/icons/icon-social-linkedin.svg" width="70">
+				</a>
+			</div>
+		</div>
+
 		<?php wp_footer(); ?>
 
 	</body>
