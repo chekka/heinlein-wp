e3ca44dc0c9da0410fe9bf525d1df2d29ae1e776
jgruber <jgruber@d-ht.com>
2024-09-02 12:29:58 +0200

    2024-09-02--1229

--- htdocs/wp-content/themes/heinlein/js/produktanfrage.js
+++ htdocs/wp-content/themes/heinlein/js/produktanfrage.js
@@ -1,6 +1,5 @@
-(function($){
-  $( '.toggle.send-box' ).on( 'click', function(){
-    $( '.ui-accordion-content-active input, .ui-accordion-content-active select' ).prop( 'disabled', false );
+(function ($) {
+  $(document).ready(function () {
+    $('.ui-accordion-content-active input, .ui-accordion-content-active select').prop('disabled', false);
   });
-})(jQuery)
-      
+})(jQuery);
